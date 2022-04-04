00a5053ff3e09f9cc7303d4e3a9a880c186fe79c
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-04-04 20:38:00 +0100

    Update products_factory.py

--- common/factories/products_factory.py
+++ common/factories/products_factory.py
@@ -9,14 +9,11 @@ from common.models.product import Product
 class ProductsFactory(object):
 
 
-    def _get_product_id():
-        return uuid4()
-
-
-    def create_product(self, name: str, description: str = None, unit: str = Units.UNIT, tags: list = []):
+    @staticmethod
+    def create_product(name: str, description: str = None, unit: str = Units.UNIT, tags: list = []):
         if not isinstance(unit, Units):
             raise InvalidParameterType()
 
-        id = self._get_product_id()
+        id = uuid4()
 
         return Product(id, name, description, unit, tags)
