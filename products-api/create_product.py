cbf31455f3b6c8eb99c34a66ea06460f67194af4
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-04-04 20:38:54 +0100

    Update create_product.py

--- products-api/create_product.py
+++ products-api/create_product.py
@@ -8,8 +8,7 @@ from products_table import ProductsTable
 def handler(event, context):
     request_body: dict = handle_request_body(event["body"])
 
-    products_factory = ProductsFactory()
-    product: Product = products_factory.create_product(**request_body)
+    product: Product = ProductsFactory.create_product(**request_body)
 
     products_table = ProductsTable()
     products_table.save_product(product)
