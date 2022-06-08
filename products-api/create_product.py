9c606da098cca4731659e916053ee7cbf0320c5d
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-06-08 21:45:16 +0100

    Updated structure

--- products-api/create_product.py
+++ products-api/create_product.py
@@ -1,7 +1,7 @@
 from dataclasses import asdict
+from uuid import uuid4
 
 from common.api import handle_request_body, handle_response
-from common.factories.products_factory import ProductsFactory
 from common.models.product import Product
 from products_table import ProductsTable
 
@@ -9,7 +9,7 @@ from products_table import ProductsTable
 def handler(event, context):
     request_body: dict = handle_request_body(event["body"])
 
-    product: Product = ProductsFactory.create_product(**request_body)
+    product: Product = Product(id=str(uuid4()), **request_body)
 
     products_table = ProductsTable()
     products_table.save_product(product)
