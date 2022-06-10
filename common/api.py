9d1f390ea3a4e42667c18972cb9e7d9c90b0999a
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-06-10 16:27:02 +0100

    Fix bug on building response

--- common/api.py
+++ common/api.py
@@ -23,10 +23,14 @@ def handle_response(event: dict, status_code: int, body: dict = None):
     response["status_code"] = status_code
 
     if body is not None:
-        response["body"] = json.dumps(body)
+        response["body"] = body
 
     logger.info(f"Response to be sent: {json.dumps(response)}")
     transformed_response = TransformDictionary.update_naming_convention(response, NamingConventions.SNAKE, NamingConventions.CAMEL)
     logger.info(f"Sending (transformed) response: {json.dumps(transformed_response)}")
+
+    transformed_body = transformed_response.get("body", None)
+    if transformed_body is not None:
+        transformed_response["body"] = json.dumps(transformed_body)
 
     return transformed_response
--- common/models/product.py
+++ common/models/product.py
@@ -7,7 +7,7 @@ class Product(object):
     name: str
     type: str
     unit: str
-    pictures: list[str] = field(init=False, repr=False, default_factory=list)
+    pictures: list[str] = field(init=False, default_factory=list)
     brand: str = field(default="Unknown")
     description: str = field(default=None)
     tags: list[str] = field(default_factory=list)
